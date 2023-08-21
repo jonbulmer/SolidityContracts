ed69b0507a3baa20bb0f2cdbedee71aa2010dcdb
Jon Bulmer <youremail@domain.com>
2023-08-21 09:51:47 +0100

    Changes to the migration and a test script

--- src/contracts/migrations/2_deploy_contracts.js
+++ src/contracts/migrations/2_deploy_contracts.js
@@ -1,5 +1,9 @@
+// Use BigNumber
+const decimals = web3.utils.toBN(18);
+const totalSupply = web3.utils.toBN(20000000000);
+
 const TIMEDividend = artifacts.require('TIMEDividend');
-const supply = 20000000000;
+const supply = totalSupply.mul(web3.utils.toBN(10).pow(decimals));
 module.exports = function (deployer) {
     deployer.deploy(TIMEDividend, supply);
 }
